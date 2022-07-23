cd9dfe1399dc93a04777803433344f64e7c9d34c
centmount <nakayama1s@kb4.so-net.ne.jp>
2022-07-23 12:21:23 +0900

    modify URL

--- main.py
+++ main.py
@@ -184,5 +184,5 @@ for word in trend_data[:5]:
     st.write("記事検索結果：", data["totalResults"])
     if data["totalResults"] > 0:
         df2 = pd.DataFrame(data["articles"])
-        st.markdown(pd.to_datetime(df2["publishedAt"], format="%Y/%m/%d %H:%M:%S"), \
-            [df2["title"]](df2["url"]))
+        st.markdown(pd.to_datetime(df2["publishedAt"], format="%Y/%m/%d %H:%M:%S"))
+        st.markdown([df2["title"]](df2["url"]))
